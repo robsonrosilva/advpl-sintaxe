35cbfe3007eebd8537b7ae01742e600fdf6c677e
Robson Rogerio Silva <robson.rogerio@poupex.com.br>
2018-10-03 19:07:11 -0300

    Implementado o Merge para Branches.

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-//import * as spawnCommand from 'spawn-command';
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage('Validação ADVPL Ativa!');
     vscode.workspace.onDidChangeTextDocument(validaADVPL);
+
+    //Adiciona comando de envia para Validação
+    let disposableVal = vscode.commands.registerCommand('advpl-sintax.gitValidacao', () => {
+        gitValidacao();
+    });
+    context.subscriptions.push(disposableVal);
+    //Adiciona comando de envia para Validação
+    let disposableRel = vscode.commands.registerCommand('advpl-sintax.gitRelease', () => {
+        gitRelease();
+    });
+    context.subscriptions.push(disposableRel);
+    //Adiciona comando de envia para Validação
+    let disposable = vscode.commands.registerCommand('advpl-sintax.gitMaster', () => {
+        gitMaster();
+    });
+    context.subscriptions.push(disposable);
 
     //Cria um colection para os erros ADVPL
     const collection = vscode.languages.createDiagnosticCollection('advpl');
@@ -42,8 +56,8 @@ export function activate(context: vscode.ExtensionContext) {
                         cBeginSql = true;
                     }
                     if (!cBeginSql && linha.toUpperCase().search("SELECT") !== -1) {
-                        aErros.push(new vscode.Diagnostic(new vscode.Range(parseInt(key), 0, parseInt(key), 0), 
-                        'Uso INDEVIDO de Query sem o Embedded SQL.! => Utilizar: BeginSQL … EndSQL.'));
+                        aErros.push(new vscode.Diagnostic(new vscode.Range(parseInt(key), 0, parseInt(key), 0),
+                            'Uso INDEVIDO de Query sem o Embedded SQL.! => Utilizar: BeginSQL … EndSQL.'));
                     }
                     if (linha.toUpperCase().search("FROM") !== -1) {
                         FromQuery = true;
@@ -52,12 +66,12 @@ export function activate(context: vscode.ExtensionContext) {
                         FromQuery = false;
                     }
                     if (FromQuery && linha.toUpperCase().search("PROTHEUS") !== -1) {
-                        aErros.push(new vscode.Diagnostic(new vscode.Range(parseInt(key), 0, parseInt(key), 0), 
-                        'Uso NÃO PERMITIDO do SHEMA PROTHEUS em Query. '));
+                        aErros.push(new vscode.Diagnostic(new vscode.Range(parseInt(key), 0, parseInt(key), 0),
+                            'Uso NÃO PERMITIDO do SHEMA PROTHEUS em Query. '));
                     }
                     if (linha.toUpperCase().search("CONOUT") !== -1) {
-                        aErros.push(new vscode.Diagnostic(new vscode.Range(parseInt(key), 0, parseInt(key), 0), 
-                        'Uso NÃO PERMITIDO do Conout. => Utilizar a API de Log padrão (FWLogMsg).'));
+                        aErros.push(new vscode.Diagnostic(new vscode.Range(parseInt(key), 0, parseInt(key), 0),
+                            'Uso NÃO PERMITIDO do Conout. => Utilizar a API de Log padrão (FWLogMsg).'));
                     }
                 }
                 if (!includeTotvs) {
@@ -65,21 +79,102 @@ export function activate(context: vscode.ExtensionContext) {
                 }
 
                 collection.set(e.document.uri, aErros);
-                /*
-                spawnCommand.child = spawnCommand('cd ' + vscode.workspace.rootPath);
-                spawnCommand.child = spawnCommand('git pull');
-
-                spawnCommand.child.stdout.on('data', function (data: any) {
-                    console.log('data', data);
-                });
-
-                spawnCommand.child.on('exit', function (exitCode: any) {
-                    console.log('exit', exitCode);
-                });*/
             }
         }
 
     }
+}
+function gitValidacao() {
+    let repository = getRepository();
+    if (!repository) {
+        return;
+    }
+
+    let branchAtual = repository.headLabel;
+    merge(repository, branchAtual, 'V11_Validacao', false);
+}
+function gitRelease() {
+    let repository = getRepository();
+    if (!repository) {
+        return;
+    }
+
+    let branchAtual = repository.headLabel;
+    merge(repository, branchAtual, 'V11_Validacao', true);
+}
+function merge(repository: any, branchAtual: any, branchdestino: any, enviaRelease: boolean) {
+    let branchesControladas = ['V11_RELEASE', 'V11_VALIDACAO', 'MASTER'];
+
+    //verifica se não está numa branch controlada
+    if (branchesControladas.indexOf(branchAtual.toUpperCase) === 0) {
+        vscode.window.showErrorMessage(
+            'Essa branch não pode ser utilizada para para Merge!'
+        );
+        return;
+    }
+
+    repository.checkout(branchdestino).then((value: any) => {
+        repository.pull().then((value: any) => {
+            repository.merge(branchAtual, "").then((value: any) => {
+                repository.push().then((value: any) => {
+                    if (enviaRelease) {
+                        repository.checkout(branchAtual);
+                        merge(repository, branchAtual, 'V11_Release', false);
+                    } else {
+                        repository.checkout(branchAtual);
+                        sucesso("", "Merge de finalizado " + repository.headLabel);
+                    }
+                    return;
+                }).catch(function () {
+                    falha(repository.headLabel + " " + arguments[0]);
+                    repository.checkout(branchAtual);
+                    return;
+                });
+            }).catch(function () {
+                falha(repository.headLabel + " " + arguments[0]);
+                repository.checkout(branchAtual);
+                return;
+            });
+        }).catch(function () {
+            falha(repository.headLabel + " " + arguments[0]);
+            repository.checkout(branchAtual);
+            return;
+        });
+    }).catch(function () {
+        falha(repository.headLabel + " " + arguments[0]);
+        repository.checkout(branchAtual);
+        return;
+    });
+}
+
+function gitMaster() {
+}
+function getRepository() {
+    if (vscode) {
+        let git = vscode.extensions.getExtension('vscode.git');
+        if (git) {
+            if (git.isActive) {
+                if (vscode.window.activeTextEditor) {
+                    let repository = git.exports._model.getRepository(vscode.window.activeTextEditor.document.uri);
+                    // set resource groups
+                    if (repository.mergeGroup.resourceStates.length !== 0 ||
+                        repository.indexGroup.resourceStates.length !== 0 ||
+                        repository.workingTreeGroup.resourceStates.length !== 0) {
+                        vscode.window.showErrorMessage("Merge não realizado, existem arquivos não commitados!");
+                        return;
+                    }
+                    return repository;
+                }
+            }
+        }
+    }
+}
+
+function sucesso(value: any, rotina: String) {
+    vscode.window.showInformationMessage('FUNCIONOU ' + rotina + " [" + value + "]");
+}
+function falha(rotina: String) {
+    vscode.window.showErrorMessage('ERRO ' + rotina + "!");
 }
 // this method is called when your extension is deactivated
 export function deactivate() {
