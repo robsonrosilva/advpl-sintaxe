183b76a391abfca40f88975bde62d47c7b707e33
Robson Rogério Silva <robson_rogerio@hotmail.com.br>
2020-03-29 01:49:04 -0300

    Melhoria de identação.

--- src/formatting.ts
+++ src/formatting.ts
@@ -5,48 +5,87 @@ import {
   CancellationToken,
   ProviderResult,
   TextEdit,
-  DocumentRangeFormattingEditProvider
+  DocumentRangeFormattingEditProvider,
+  Range,
+  Position
 } from 'vscode';
 import { FormattingRules, RuleMatch } from './formmatingRules';
 
+// Regras de estruturas que não sofrem identação interna
+let structsNoIdent: string[] = [
+  'beginsql (alias)?',
+  'comment',
+  'protheus doc',
+  'begin content',
+  'no format'
+];
+
 class Formatting implements DocumentFormattingEditProvider {
   lineContinue: boolean = false;
-  // Regras de estruturas que não sofrem identação interna
-  structsNoIdent: string[] = [
-    'beginsql (alias)?',
-    'Comentários',
-    'Protheus Doc'
-  ];
 
   provideDocumentFormattingEdits(
     document: TextDocument,
     options: FormattingOptions,
     token: CancellationToken
   ): ProviderResult<TextEdit[]> {
-    const formattingRules = new FormattingRules();
+    const formatter: RangeFormatting = new RangeFormatting();
+    // define o range como todo o documento
+    const range: Range = new Range(
+      new Position(0, 0),
+      new Position(document.lineCount - 1, 0)
+    );
+    return formatter.provideDocumentRangeFormattingEdits(
+      document,
+      range,
+      options,
+      token
+    );
+  }
+}
+
+class RangeFormatting implements DocumentRangeFormattingEditProvider {
+  lineContinue: boolean = false;
+  provideDocumentRangeFormattingEdits(
+    document: TextDocument,
+    range: Range,
+    options: FormattingOptions,
+    token: CancellationToken
+  ): ProviderResult<TextEdit[]> {
+    let cont: number = 0;
     const tab: string = options.insertSpaces
       ? ' '.repeat(options.tabSize)
       : '\t';
-    let identBlock: string = '';
-    let cont: number = 0;
+    // define comom está a identação quando é identação range
+    if (range.start.line > 0) {
+      let stringStart = document
+        .lineAt(range.start.line)
+        .text.replace(/(^\s*)(.*)/, '$1');
+      while (stringStart.search(tab) >= 0) {
+        cont++;
+        stringStart = stringStart.replace(tab, '');
+      }
+    }
+    const formattingRules = new FormattingRules();
+    let identBlock: string = tab.repeat(cont);
 
     let result: TextEdit[] = [];
-    const lc = document.lineCount;
+    const lc = range.end.line;
     const rulesIgnored: any[] = formattingRules
       .getClosedStructures()
       .filter(rule => {
-        return this.structsNoIdent.indexOf(rule.id) !== -1;
+        return structsNoIdent.indexOf(rule.id) !== -1;
       });
 
-    for (let nl = 0; nl < lc; nl++) {
+    for (let nl = range.start.line; nl <= lc; nl++) {
+      console.log(nl);
       const line = document.lineAt(nl);
       const text = line.text.trimRight();
-      let lastRule: string =
+      let lastRule: RuleMatch =
         formattingRules.openStructures[
           formattingRules.openStructures.length - 1
         ];
       let foundIgnore: any[] = rulesIgnored.filter(rule => {
-        return rule.id === lastRule;
+        return lastRule && lastRule.rule && rule.id === lastRule.rule.id;
       });
       // dentro do BeginSql não mexe na identação
       if (foundIgnore.length > 0 && !text.match(foundIgnore[0].end)) {
@@ -61,9 +100,18 @@ class Formatting implements DocumentFormattingEditProvider {
 
           if (ruleMatch) {
             if (ruleMatch.decrement) {
-              cont = cont < 1 ? 0 : cont - 1;
+              if (ruleMatch.decrementDouble) {
+                cont = cont < 2 ? 0 : cont - 2;
+              } else {
+                cont = cont < 1 ? 0 : cont - 1;
+              }
               identBlock = tab.repeat(cont);
             }
+          }
+
+          if (ruleMatch.incrementDouble) {
+            cont += 1;
+            identBlock = tab.repeat(cont);
           }
 
           const newLine: string = text.replace(/(\s*)?/, identBlock);
@@ -72,11 +120,10 @@ class Formatting implements DocumentFormattingEditProvider {
             newLine
               .split('//')[0]
               .trim()
-              .endsWith(';') &&
-            ['Comentários', 'Protheus Doc'].indexOf(ruleMatch.rule.id) === -1;
+              .endsWith(';') && rulesIgnored.indexOf(ruleMatch.rule.id) === -1;
 
           if (ruleMatch) {
-            if (ruleMatch.increment) {
+            if (ruleMatch.increment || ruleMatch.incrementDouble) {
               cont++;
               identBlock = tab.repeat(cont);
             }
@@ -98,17 +145,6 @@ class Formatting implements DocumentFormattingEditProvider {
     }
 
     return result;
-  }
-}
-
-class RangeFormatting implements DocumentRangeFormattingEditProvider {
-  provideDocumentRangeFormattingEdits(
-    document: TextDocument,
-    range: import('vscode').Range,
-    options: FormattingOptions,
-    token: CancellationToken
-  ): ProviderResult<TextEdit[]> {
-    throw new Error('Method not implemented.');
   }
 }
 
